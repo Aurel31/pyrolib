d17da7a6fb70a4c41c98f8e3ef7537790004f701
Aurel31 <aurelien.costes31@gmail.com>
2022-02-24 19:12:01 +0100

    Add fuel management for FuelMap.nc file

---
+++ src/pyrolib/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+__author__ = "Aurélien Costes"
+__credits__ = ["Aurélien Costes"]
+__license__ = "MIT"
+__version__ = "0.0.0"
+__maintainer__ = "Aurélien Costes"
+__email__ = "aurelien.costes31@gmail.com"
+__all__=['fuels', 'firefluxpost', 'blaze']
