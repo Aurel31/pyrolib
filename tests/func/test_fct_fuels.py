646419eebc3e2c2bce5b5292905a744fcda0bcfa
Aurel31 <aurelien.costes31@gmail.com>
2022-04-27 19:35:19 +0200

    update tests

--- tests/func/test_fct_fuels.py
+++ tests/func/test_fct_fuels.py
@@ -8,17 +8,18 @@ import pytest
 
 
 def test_simple_case_ff():
-    scenario = pl.Scenario(load='FireFluxI')
-    fuelmap = pl.FuelMap(scenario=scenario, workdir=f"{os.getcwd()}/examples")
+    my_db = pl.FuelDatabase()
+    my_db.load_fuel_database("FireFluxI")
+    fuelmap = pl.FuelMap(fuel_db=my_db, workdir=f"{os.getcwd()}/examples")
 
-    fuelmap.add_rectangle_patch_fuel(xpos=[50.0, 450.0], ypos=[50.0, 450.0], fuel_index=1)
-    fuelmap.add_rectangle_patch_ignition(xpos=[100.0, 105.0], ypos=[245.0, 255.0], ignition_time=10.0)
-    fuelmap.add_rectangle_patch_unburnable(xpos=[100.0, 150.0], ypos=[50.0, 150.0])
+    fuelmap.add_fuel_rectangle_patch(xpos=[50.0, 450.0], ypos=[50.0, 450.0], fuel_key="FireFluxI_tall_grass")
+    fuelmap.add_ignition_rectangle_patch(xpos=[100.0, 105.0], ypos=[245.0, 255.0], ignition_time=10.0)
+    fuelmap.add_unburnable_rectangle_patch(xpos=[100.0, 150.0], ypos=[50.0, 150.0])
 
-    fuelmap.add_line_patch_fuel(xpos=[50.0, 450.0], ypos=[50.0, 450.0], fuel_index=1)
-    fuelmap.add_line_patch_ignition(xpos=[100.0, 105.0], ypos=[245.0, 255.0], ignition_time=10.0)
-    fuelmap.add_line_patch_unburnable(xpos=[100.0, 150.0], ypos=[50.0, 150.0])
-    fuelmap.add_line_patch_walking_ignition([200.0, 200.0], [300.0, 350.0], walking_ignition_times=[0.0, 100.0])
+    fuelmap.add_fuel_line_patch(xpos=[50.0, 450.0], ypos=[50.0, 450.0], fuel_key="FireFluxI_tall_grass")
+    fuelmap.add_ignition_line_patch(xpos=[100.0, 105.0], ypos=[245.0, 255.0], ignition_time=10.0)
+    fuelmap.add_unburnable_line_patch(xpos=[100.0, 150.0], ypos=[50.0, 150.0])
+    fuelmap.add_walking_ignition_line_patch([200.0, 200.0], [300.0, 350.0], walking_ignition_times=[0.0, 100.0])
 
     fuelmap.dump_mesonh()
     assert os.path.exists(f"{os.getcwd()}/examples/FuelMap.nc")
