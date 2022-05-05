5c2930015c4d2ab364906324ab198a9f40a9b661
Aurel31 <aurelien.costes31@gmail.com>
2022-05-05 15:44:50 +0200

    remove tmp gamma_x/y values for cli_post

--- src/pyrolib/cli_post.py
+++ src/pyrolib/cli_post.py
@@ -83,8 +83,6 @@ def rearrange_netcdf(files, remove):
                         .split(",")[1]
                         .replace(")", "")
                     )
-                    gamma_x = 5
-                    gamma_y = 5
                     # fire grid size
                     Lmax = gamma_x * len(grid_u_x)
                     Mmax = gamma_y * len(grid_v_y)
